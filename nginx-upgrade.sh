d7afc92e3bb129ec014ef15284d5e661932336c6
terenceamas <89087574+terenceamas@users.noreply.github.com>
2024-02-03 11:35:00 +0800

    Update nginx-upgrade.sh

--- nginx-upgrade.sh
+++ nginx-upgrade.sh
@@ -1,5 +1,6 @@
-echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+echo "deb https://nginx.org/packages/ubuntu/ `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+echo "deb-src https://nginx.org/packages/ubuntu/ `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+#echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
 curl -o /tmp/nginx_signing.key https://nginx.org/keys/nginx_signing.key
 sudo mv /tmp/nginx_signing.key /etc/apt/trusted.gpg.d/nginx_signing.asc
 
